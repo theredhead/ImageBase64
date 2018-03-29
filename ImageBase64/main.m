7509d216d46dbee6c813f8c0a07aef40fd2ba54b
Kris Herlaar <kris@theredhead.nl>
2018-03-29 18:25:25 +0200

    Initial Commit

---
+++ ImageBase64/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ImageBase64
+//
+//  Created by Kris Herlaar on 29/03/2018.
+//  Copyright © 2018 Kris Herlaar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
---
+++ ImageBase64Tests/ImageBase64Tests.m
@@ -0,0 +1,39 @@
+//
+//  ImageBase64Tests.m
+//  ImageBase64Tests
+//
+//  Created by Kris Herlaar on 29/03/2018.
+//  Copyright © 2018 Kris Herlaar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageBase64Tests : XCTestCase
+
+@end
+
+@implementation ImageBase64Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
