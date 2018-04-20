6a6d520db1335a61432d8a5db6a5329976c098ee
Kris Herlaar <kris@theredhead.nl>
2018-04-20 20:45:32 +0200

    Initial commit, working app

--- ImageBase64/AppDelegate.m
+++ ImageBase64/AppDelegate.m
@@ -10,7 +10,11 @@
 
 @interface AppDelegate ()
 
+- (IBAction)imageDropped:(NSImageView *)sender;
+	
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *instructionsLabel;
+	
 @end
 
 @implementation AppDelegate
@@ -19,10 +23,26 @@
 	// Insert code here to initialize your application
 }
 
-
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
 	// Insert code here to tear down your application
 }
 
+- (IBAction)imageDropped:(NSImageView *)sender {
+	
+	NSBitmapImageRep* rep = 
+		[NSBitmapImageRep imageRepWithData:[[sender image] TIFFRepresentation]];
+	
+	NSData* data = [rep representationUsingType:NSPNGFileType properties: [[NSDictionary alloc] init]];
+	
+	NSString * encoded = [data base64EncodedStringWithOptions:0];
 
+	NSString * result = [@"data:image/png;base64," stringByAppendingString:encoded];
+	
+	NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+	[pasteboard clearContents];
+	[pasteboard writeObjects:@[result]];
+	
+	[_instructionsLabel setStringValue:@"Base64 url copied."];
+}
+	
 @end
---
+++ ImageBase64/image64.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  image64.sh
+#  ImageBase64
+#
+#  Created by Kris Herlaar on 29/03/2018.
+#  Copyright © 2018 Kris Herlaar. All rights reserved.
